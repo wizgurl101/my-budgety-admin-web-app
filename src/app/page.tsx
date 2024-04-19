0be0183af856b5ef19fa7c89b0e1acdf1b207470
wizgurl101 <nb.phan@hotmail.com>
2024-04-19 12:57:57 -0600

    fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Button, TextField, Container, Typography} from '@mui/material';
+import NavBar from "@/components/navbar/page";
 export default function Home() {
     return (
+        <>
+        <NavBar />
         <Container
             sx={{
                 display: 'flex',
@@ -10,31 +13,45 @@ export default function Home() {
                 justifyContent: 'center',
                 height: '100vh',
                 backgroundColor: '#fff',
+                gap: 2
             }}
         >
             <Typography
                 variant="h4"
                 sx={{
-                    color: 'black'
+                    color: '#97C67C',
+                    mb: 6
                 }}
             >
                 MY BUDGETY
             </Typography>
             <TextField
+                required
                 label="Email"
                 variant="outlined"
+                sx={{
+                    width: '40%'
+                }}
             />
             <TextField
+                required
                 label="Password"
                 type="password"
                 variant="outlined"
+                sx={{
+                    width: '40%'
+                }}
             />
             <Button
                 variant="contained"
                 color="primary"
+                sx={{
+                    width: '40%'
+                }}
             >
                 SIGN IN
             </Button>
         </Container>
+        </>
     );
 }
