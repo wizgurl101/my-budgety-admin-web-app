ff9e20d5b92f5b8ac276acc5d70997ae3704c81f
wizgurl101 <nb.phan@hotmail.com>
2024-04-19 12:05:30 -0600

    implement login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import React from 'react';
+import { Button, TextField, Container, Typography} from '@mui/material';
 export default function Home() {
-  return (
-    <div>
-      My Budget
-    </div>
-  );
+    return (
+        <Container
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+                backgroundColor: '#fff',
+            }}
+        >
+            <Typography
+                variant="h4"
+                sx={{
+                    color: 'black'
+                }}
+            >
+                MY BUDGETY
+            </Typography>
+            <TextField
+                label="Email"
+                variant="outlined"
+            />
+            <TextField
+                label="Password"
+                type="password"
+                variant="outlined"
+            />
+            <Button
+                variant="contained"
+                color="primary"
+            >
+                SIGN IN
+            </Button>
+        </Container>
+    );
 }
