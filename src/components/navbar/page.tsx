f80226773a37b73603caaee6afccda8d9d3ed387
wizgurl101 <nb.phan@hotmail.com>
2024-10-13 17:32:15 -0600

    add menu icon to nav bar

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,30 +1,50 @@
+'use client'
+
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SideNav from "@/components/sidenav/page";
+
 export default function NavBar() : React.JSX.Element {
+    const [openSideNav, setOpenSideNav] = React.useState<boolean>(false);
+
+    const setSideNav = (): void => {
+        setOpenSideNav(false);
+    }
+
     return (
-        <AppBar
-            position="static"
-            sx={{
-                backgroundColor: '#3F3F44'
-                }}
-        >
-        <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-            }}
-        >
-            MY BUDGETY
-        </Typography>
-    </AppBar>
+        <>
+            <AppBar
+                position="static"
+                sx={{
+                    backgroundColor: '#3F3F44'
+                    }}
+            >
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        MY BUDGETY
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <SideNav
+                onClose={setSideNav}
+                open={openSideNav}
+            />
+        </>
     );
 }
