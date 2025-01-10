8c9b1ed11c20d97494af6158d9a38a3d48456f05
wizgurl101 <nb.phan@hotmail.com>
2025-01-10 12:55:51 -0700

    add no-cors

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -2,22 +2,43 @@
 
 import * as React from 'react';
 import useSWR from 'swr'
-import { DataGrid } from '@mui/x-data-grid';
+import {DataGrid} from '@mui/x-data-grid';
 
 const columns = [
     { field: 'category_id', headerName: 'Category ID', width: 200 },
     { field: 'name', headerName: 'Name', width: 200 },
 ]
 
+const fetcher = async (url: string) => {
+    try
+    {
+        const res = await fetch(url, {
+            mode: 'no-cors'
+        })
+
+        if(!res.ok)
+        {
+            throw new Error('Network response to get all category was not ok')
+        }
+
+        return await res.json()
+    } catch (error) {
+        // @ts-ignore
+        throw new Error(`Failed to fetch data: ${error.message}`)
+    }
+}
+
 export default function Page(): React.JSX.Element {
-    const fetcher = (url: string) => fetch(url).then(res => res.json())
     const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_GET_ALL_CATEGORY_LOCALHOST_URL}`, fetcher)
 
     if (error) return <div>Error loading data</div>
     if (isLoading) return <div>Loading...</div>
 
+    console.log(JSON.stringify(data.data))
+
     return (
         <>
+            <h4>Category</h4>
             <DataGrid rows={data.data} columns={columns} />
         </>
     )
---
+++ src/app/dashboard/expanses/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import * as React from 'react';
+import useSWR from 'swr'
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+    { field: 'category_id', headerName: 'Category ID', width: 200 },
+    { field: 'name', headerName: 'Name', width: 200 },
+]
+
+export default function Page(): React.JSX.Element {
+    const fetcher = (url: string) => fetch(url).then(res => res.json())
+    const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_GET_MONTH_EXPANSE_LOCALHOST_URL_LOCALHOST_URL}`, fetcher)
+
+    if (error) return <div>Error loading data</div>
+    if (isLoading) return <div>Loading...</div>
+
+    return (
+        <>
+            <DataGrid rows={data.data} columns={columns} />
+        </>
+    )
+}
