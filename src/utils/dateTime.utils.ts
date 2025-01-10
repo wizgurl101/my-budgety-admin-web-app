78f6b7b7e2d26f13766ec8a17423f98357cc90d6
wizgurl101 <nb.phan@hotmail.com>
2025-01-10 16:54:12 -0700

    move expanse table to main dashboard page

---
+++ src/utils/dateTime.utils.ts
@@ -0,0 +1,5 @@
+// export const getCurrentMonthFirstDay = () :string => {
+//     const date = new Date();
+//     const firstDay =  new Date(date.getFullYear(), date.getMonth(), 1).toISOString();
+//     return `${firstDay.fullYear()}-${firstDay.month()}-${firstDay.date()}`;
+// }
