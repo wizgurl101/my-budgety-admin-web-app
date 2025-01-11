a3b510fab386b757e17fbc8bbedde7c175430214
wizgurl101 <nb.phan@hotmail.com>
2025-01-10 19:47:23 -0700

    add datetime utils
    add functions to return the first date and last date of a given month

--- src/utils/dateTime.utils.ts
+++ src/utils/dateTime.utils.ts
@@ -1,5 +1,31 @@
-// export const getCurrentMonthFirstDay = () :string => {
-//     const date = new Date();
-//     const firstDay =  new Date(date.getFullYear(), date.getMonth(), 1).toISOString();
-//     return `${firstDay.fullYear()}-${firstDay.month()}-${firstDay.date()}`;
-// }
+function getDateStr(dateNumber: number): string {
+    let str = dateNumber.toString();
+    if (dateNumber < 10) {
+        str = `0${str}`;
+    }
+    return str;
+}
+
+export const getMonthFirstDay = (date: Date) :string => {
+    const firstDay =  new Date(date.getFullYear(), date.getMonth(), 1);
+
+    const firstDayMonth = firstDay.getMonth() + 1;
+    const firstDayMonthStr = getDateStr(firstDayMonth);
+
+    const firstDayDate = firstDay.getDate();
+    const firstDayDateStr = getDateStr(firstDayDate);
+
+    return `${firstDay.getFullYear()}-${firstDayMonthStr}-${firstDayDateStr}`;
+}
+
+export const getMonthLastDay = (date: Date) :string => {
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+    const lastDayMonth = lastDay.getMonth() + 1;
+    const lastDayMonthStr = getDateStr(lastDayMonth);
+
+    const lastDayDate = lastDay.getDate();
+    const lastDayDateStr = getDateStr(lastDayDate);
+
+    return `${lastDay.getFullYear()}-${lastDayMonthStr}-${lastDayDateStr}`
+}
