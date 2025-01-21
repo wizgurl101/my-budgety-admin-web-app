fa64e5b2614837d7d0d4267349cd3ddce46bf79e
wizgurl101 <nb.phan@hotmail.com>
2025-01-21 09:52:46 -0700

    implement logic to upload file

--- src/app/dashboard/uploadBudget/page.tsx
+++ src/app/dashboard/uploadBudget/page.tsx
@@ -7,9 +7,41 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import useSWR from 'swr';
+import LoadingBar from "../../../components/loadingBar/page";
 
 export default function UploadBudget() {
     const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
+    const [showMessage, setShowMessage] = React.useState(false);
+    const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+    //todo look into context API to save the User ID
+    const userID = process.env.NEXT_PUBLIC_USER_ID
+
+    const fetcher = async () => {
+        const formData = new FormData();
+
+        formData.append('file', selectedFile as File);
+
+        const url = `http://localhost:5000/uploadToExpanse/csv?userId=${userID}`;
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData,
+        });
+
+        if (!response.ok) {
+            throw new Error('An error occurred while uploading the file');
+        }
+
+        setShowMessage(true);
+
+        return response.json();
+    };
+
+    const { data, error, isLoading, mutate } = useSWR(
+        selectedFile ? 'upload-budget' : null,
+        fetcher,
+        { revalidateOnFocus: false, revalidateOnReconnect: false }
+    );
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files.length > 0) {
@@ -17,35 +49,19 @@ export default function UploadBudget() {
         }
     };
 
-    const fetcher = async (url: string, file: File) => {
-        const formData = new FormData();
-
-        console.log(file.name)
-        formData.append('file', file);
-
-        const response = await fetch(url, {
-            method: 'POST',
-            body: formData,
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to upload file');
-        }
-
-        return response.json();
-    };
-
-    const { data, error, mutate } = useSWR(
-        selectedFile ? [`${process.env.NEXT_PUBLIC_UPLOAD_CSV_TO_EXPANSE_LOCALHOST_URL}`, selectedFile] : null,
-        fetcher
-    );
-
-    const handleUpload = () => {
+    const handleUpload = async () => {
         if (!selectedFile) {
             alert('Please select a file first.');
             return;
         }
-        mutate();
+
+        await mutate();
+
+        setSelectedFile(null);
+
+        setTimeout(() => {
+            setShowMessage(false);
+        }, 3000);
     };
 
     return (
@@ -60,17 +76,30 @@ export default function UploadBudget() {
                         variant="outlined"
                         fullWidth
                         onChange={handleFileChange}
-                        inputProps={{ accept: '.csv' }}
+                        disabled={isLoading}
+                        inputRef={fileInputRef}
                     />
                     <Button
                         variant="contained"
                         color="primary"
                         onClick={handleUpload}
                         sx={{ mt: 2 }}
+                        disabled={isLoading}
                     >
                         Upload
                     </Button>
                 </Grid>
+                {showMessage && (
+                    <Grid size={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Typography variant="body1">File uploaded successfully</Typography>
+                    </Grid>
+                )}
+                {error && (
+                    <Grid size={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Typography variant="body1">Error with uploading file</Typography>
+                    </Grid>
+                )}
+                {isLoading && (<LoadingBar />)}
             </Grid>
         </Box>
     )
