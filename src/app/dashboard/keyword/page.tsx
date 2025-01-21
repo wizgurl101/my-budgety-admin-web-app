44347dc4fec93159b863d96277841a04425a8180
wizgurl101 <nb.phan@hotmail.com>
2025-01-20 19:50:53 -0700

    implement select category menu

--- src/app/dashboard/keyword/page.tsx
+++ src/app/dashboard/keyword/page.tsx
@@ -7,6 +7,8 @@ import {DataGrid} from '@mui/x-data-grid';
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
+import { MenuItem, FormControl, InputLabel } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Button from '@mui/material/Button';
 import IconButton from "@mui/material/IconButton";
 import EditIcon from "@mui/icons-material/Edit";
@@ -14,11 +16,48 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import {fetcher} from "@/utils/SWR.utils";
 
 export default function Page(): React.JSX.Element {
+    const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_GET_ALL_CATEGORY_LOCALHOST_URL}`, fetcher)
+    const [selectedCategoryId, setSelectedCategoryId] = React.useState('');
+
+    if (error){
+        return  <Typography variant="h4">Error Loading Data</Typography>
+    }
+    if (isLoading) return <LoadingBar />
+
+    if (error){
+        return  <Typography variant="h4">Error Loading Data</Typography>
+    }
+    if (isLoading) return <LoadingBar />
+
+    const handleSelectCategory = (event: SelectChangeEvent) => {
+        setSelectedCategoryId(event.target.value as string);
+    };
+
     return (
     <Box sx={{flexGrow: 1}}>
         <Grid container spacing={12}>
-            <Grid size={10} sx={{ mt: '2rem', mb: '2rem' }}>
+            <Grid size={12} sx={{ mt: '2rem'}}>
                 <Typography variant="h4">Manage Category's Keywords</Typography>
+            </Grid>
+            <Grid size={12}>
+                <FormControl fullWidth>
+                    <InputLabel id="category-select-label"
+                    sx={{ mt: '0.6rem' }}
+                    >
+                        Select A Category
+                    </InputLabel>
+                    <Select
+                        labelId="category-select-label"
+                        value={selectedCategoryId}
+                        onChange={handleSelectCategory}
+                    >
+                        {data.map((category: { id: string, category_id: string, name: string }) => (
+                            <MenuItem key={category.id} value={category.category_id}>
+                                {category.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
             </Grid>
         </Grid>
     </Box>
