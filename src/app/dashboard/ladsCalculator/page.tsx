166d05f8c47ad42181b2ae32317a597c5c3a35f4
wizgurl101 <nb.phan@hotmail.com>
2025-05-07 15:34:34 -0600

    get lads amount spent from api

--- src/app/dashboard/ladsCalculator/page.tsx
+++ src/app/dashboard/ladsCalculator/page.tsx
@@ -1,15 +1,41 @@
 'use client';
 
 import * as React from 'react';
+import useSWR from 'swr';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
 import Typography from '@mui/material/Typography';
 
+import { fetcher } from '@/utils/SWR.utils';
+import LoadingBar from '@/components/LoadingBar/page';
+
 export default function Page(): React.JSX.Element {
+  const currentDate: Date = new Date(Date.now());
+
+  const ladsSpendURL =
+    `http://${process.env.NEXT_PUBLIC_HOST}` +
+    `/gamesSpentAmount/ladsYearAmount?userId=${process.env.NEXT_PUBLIC_USER_ID}` +
+    `&year=${currentDate.getFullYear()}`;
+
+  const { data: ladsData, error, isLoading } = useSWR(ladsSpendURL, fetcher);
+
+  if (error) {
+    return <Typography variant="h4">Error Loading Data</Typography>;
+  }
+
+  if (isLoading) {
+    return <LoadingBar />;
+  }
+
+  const ladsSpentAmount = ladsData[0].total;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Typography variant="h4">Lads Banner Cost Estimate</Typography>
+        <Typography variant="h4">
+          Total Spent in {currentDate.getFullYear()}: ${ladsSpentAmount}
+        </Typography>
       </Grid>
     </Box>
   );
