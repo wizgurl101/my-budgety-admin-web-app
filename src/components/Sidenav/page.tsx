72154892ad8c72c174f1bfc0966bbf1e35fa3bd3
wizgurl101 <nb.phan@hotmail.com>
2025-06-06 07:54:46 -0600

    style side nav

--- src/components/Sidenav/page.tsx
+++ src/components/Sidenav/page.tsx
@@ -8,6 +8,12 @@ import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import SettingsIcon from '@mui/icons-material/Settings';
+import StarIcon from '@mui/icons-material/Star';
 
 export interface SideNavProps {
   onClose?: () => void;
@@ -21,6 +27,16 @@ export default function SideNav({
   return (
     <Drawer sx={{ width: 320, maxWidth: '100%' }} onClose={onClose} open={open}>
       <MenuList>
+        <Stack direction="row" spacing={0}>
+          <AnalyticsIcon sx={{ fontSize: 40, paddingTop: 1, paddingLeft: 3 }} />
+          <Typography
+            variant="h6"
+            sx={{ paddingTop: 2, paddingBottom: 2, paddingLeft: 1 }}
+          >
+            Analyics
+          </Typography>
+        </Stack>
+        <Divider />
         <MenuItem>
           <ListItem component={Link} href={'/dashboard'}>
             <ListItemText>Dashboard</ListItemText>
@@ -31,6 +47,16 @@ export default function SideNav({
             <ListItemText>Month Expanses</ListItemText>
           </ListItem>
         </MenuItem>
+        <Stack direction="row" spacing={0}>
+          <SettingsIcon sx={{ fontSize: 40, paddingTop: 1, paddingLeft: 3 }} />
+          <Typography
+            variant="h6"
+            sx={{ paddingTop: 2, paddingBottom: 2, paddingLeft: 1 }}
+          >
+            Admin
+          </Typography>
+        </Stack>
+        <Divider />
         <MenuItem>
           <ListItem component={Link} href={'/dashboard/uploadBudget'}>
             <ListItemText>Upload Csv to Expanses</ListItemText>
@@ -46,6 +72,16 @@ export default function SideNav({
             <ListItemText>Category Keywords Management</ListItemText>
           </ListItem>
         </MenuItem>
+        <Stack direction="row" spacing={0}>
+          <StarIcon sx={{ fontSize: 40, paddingTop: 1, paddingLeft: 3 }} />
+          <Typography
+            variant="h6"
+            sx={{ paddingTop: 2, paddingBottom: 2, paddingLeft: 1 }}
+          >
+            Tools
+          </Typography>
+        </Stack>
+        <Divider />
         <MenuItem>
           <ListItem component={Link} href={'/dashboard/ladsCalculator'}>
             <ListItemText>LaDS Banner Calculator</ListItemText>
