6a77164b74199fcc8e6cf3a10338f0dc1c36922d
wizgurl101 <nb.phan@hotmail.com>
2026-04-15 12:23:12 -0600

    change character banner inputs

--- src/app/dashboard/wuwaCalculator/bannerEstimateComponent.tsx
+++ src/app/dashboard/wuwaCalculator/bannerEstimateComponent.tsx
@@ -2,70 +2,41 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import {
-  MenuItem,
-  FormControl,
-  InputLabel,
-  Stack,
-  TextField,
-  Button,
-} from '@mui/material';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { FormControl, Stack, TextField, Button } from '@mui/material';
+import { SelectChangeEvent } from '@mui/material/Select';
 import Image from 'next/image';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
 
-import { ladsBannerTypes } from '@/app/dashboard/ladsCalculator/lads.constants';
 import { calculateBannerCost } from './wuwa.helpers';
 
 export default function BannerEstimateComponent(): React.JSX.Element {
-  const [selectedBannerType, setBannerType] = React.useState('multi');
-  const [diamondNumber, setDiamondNumber] = React.useState(0);
-  const [purpleDiamondNumber, setPurpleDiamondNumber] = React.useState(0);
-  const [deepspaceWishNumber, setDeepspaceWishNumber] = React.useState(0);
+  const [asteriteNumber, setAsteriteNumber] = React.useState(0);
+  const [radiantTidesNumber, setRadiantTidesNumber] = React.useState(0);
   const [pityNumber, setPityNumber] = React.useState(0);
-  const [wishesMadeNumber, setWishesMadeNumber] = React.useState(0);
   const [bannerCost, setBannerCost] = React.useState(0);
   const [isEventCardGuaranteed, setIsEventCardGuaranteed] =
     React.useState(false);
   const [showImage, setShowImage] = React.useState(false);
   const [imageUrl, setImageUrl] = React.useState('');
 
-  const handleBannerTypeChange = (event: SelectChangeEvent) => {
-    setBannerType(event.target.value);
+  const handleAsteriteNumber = (event: React.FocusEvent<HTMLInputElement>) => {
+    const asteriteNumber = parseInt(event.target.value);
+    setAsteriteNumber(asteriteNumber);
   };
 
-  const handleDiamondNumber = (event: React.FocusEvent<HTMLInputElement>) => {
-    const diamondNumber = parseInt(event.target.value);
-    setDiamondNumber(diamondNumber);
-  };
-
-  const handlePurpleDiamondNumber = (
+  const handleRadiantTidesNumber = (
     event: React.FocusEvent<HTMLInputElement>
   ) => {
-    const purpleDiamondNumber = parseInt(event.target.value);
-    setPurpleDiamondNumber(purpleDiamondNumber);
-  };
-
-  const handleDeepspaceWishNumber = (
-    event: React.FocusEvent<HTMLInputElement>
-  ) => {
-    const deepspaceWishNumber = parseInt(event.target.value);
-    setDeepspaceWishNumber(deepspaceWishNumber);
+    const radiantTidesNumber = parseInt(event.target.value);
+    setRadiantTidesNumber(radiantTidesNumber);
   };
 
   const handlePityNumber = (event: React.FocusEvent<HTMLInputElement>) => {
     const pityNumber = parseInt(event.target.value);
     setPityNumber(pityNumber);
-  };
-
-  const handleWishesMadeNumber = (
-    event: React.FocusEvent<HTMLInputElement>
-  ) => {
-    const wishesMadeNumber = parseInt(event.target.value);
-    setWishesMadeNumber(wishesMadeNumber);
   };
 
   const handleEventCardGuaranteedChange = (
@@ -118,43 +89,19 @@ export default function BannerEstimateComponent(): React.JSX.Element {
             useFlexGap
             sx={{ flexWrap: 'wrap' }}
           >
-            <FormControl fullWidth>
-              <InputLabel id="banner-type-select-label" sx={{ mt: '0.6rem' }}>
-                Select The Type Of Banner
-              </InputLabel>
-              <Select
-                labelId="banner-type-select-label"
-                value={selectedBannerType}
-                onChange={handleBannerTypeChange}
-                sx={{ width: '500px' }}
-              >
-                {ladsBannerTypes.map((type, index) => (
-                  <MenuItem key={index} value={type}>
-                    {type}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
             <TextField
-              label="Enter number of diamonds"
+              label="Enter number of asterites"
               type="number"
               variant="outlined"
               sx={{ width: '500px' }}
-              onBlur={handleDiamondNumber}
+              onBlur={handleAsteriteNumber}
             />
             <TextField
-              label="Enter number of purple diamonds"
+              label="Enter number of radiant tides"
               type="number"
               variant="outlined"
               sx={{ width: '500px' }}
-              onBlur={handlePurpleDiamondNumber}
-            />
-            <TextField
-              label="Enter number of deepspace wish"
-              type="number"
-              variant="outlined"
-              sx={{ width: '500px' }}
-              onBlur={handleDeepspaceWishNumber}
+              onBlur={handleRadiantTidesNumber}
             />
             <TextField
               label="Enter number of pity"
@@ -162,13 +109,6 @@ export default function BannerEstimateComponent(): React.JSX.Element {
               variant="outlined"
               sx={{ width: '500px' }}
               onBlur={handlePityNumber}
-            />
-            <TextField
-              label="wishes made"
-              type="number"
-              variant="outlined"
-              sx={{ width: '500px' }}
-              onBlur={handleWishesMadeNumber}
             />
             <FormControl>
               <FormLabel id="event-card-radio-group-label">
