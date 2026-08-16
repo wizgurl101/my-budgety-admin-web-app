f9032f2ab1e5acfd5e86bd0db6a4bf387058128f
wizgurl101 <nb.phan@hotmail.com>
2026-08-16 16:18:58 -0600

    update storybook version to 10

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
-    '@storybook/addon-docs'
+    '@storybook/addon-docs',
   ],
   framework: {
     name: '@storybook/nextjs',
